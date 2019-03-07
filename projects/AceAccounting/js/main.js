deec9d46748298d00f9700ed34fe30d22e3e8357
iroro1 <ojigboleo@gmail.com>
2019-03-08 00:49:38 +0300

    First commit

---
+++ projects/AceAccounting/js/main.js
@@ -0,0 +1,37 @@
+// Initialize and add the map
+function initMap() {
+  // Your location
+  const loc = { lat: 35.070329, lng: 32.854614 };
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector(".map"), {
+    zoom: 14,
+    center: loc
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc });
+}
+
+// Sticky menu background
+window.addEventListener("scroll", function() {
+  if (window.scrollY > 150) {
+    document.querySelector("#navbar").style.opacity = 0.9;
+  } else {
+    document.querySelector("#navbar").style.opacity = 1;
+  }
+});
+
+// Smooth Scrolling
+$(".nav a, .btn").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 117
+      },
+      900
+    );
+  }
+});
